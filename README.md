fae0328b53a6f7b9fa05d62122ed664dfb3c2e05
Kristian Trenskow <this.is@kristians.email>
2025-04-25 19:02:31 +0200

    First version.

---
+++ README.md
@@ -0,0 +1,50 @@
+# OnFrame
+
+A modifier for SwiftUI to get callbacks on every frame update (based on `CADisplayLink`).
+
+## Usage
+
+The above example uses the a callback.
+
+````swift
+import SwiftUI
+import OnFrame
+
+struct SampleView: View {
+  @State var timestamp: TimeInterval
+  
+  var body: some View {
+    Text("\(self.timestamp)")
+    	.onFrame { _ /* last frame timestamp */, timestamp /* target frame timestamp */ in
+        self.timestamp = timestamp
+      }
+  }
+}
+````
+
+Another way to use it is to just add the modifier and then get values using an environment value.
+
+````swift
+import SwiftUI
+import OnFrame
+
+struct FrameView: View {
+  
+  @Environment(\.timestamp) var timestamp
+  
+  var body: some View {
+    Text("\(self.timestamp)")
+  }
+}
+
+struct MainView: View {
+  var body: some View {
+    FrameView()
+    	.onFrame()
+  }
+}
+````
+
+# License
+
+See license in LICENSE.
--- Sources/OnFrame/OnFrame.swift
+++ Sources/OnFrame/OnFrame.swift
@@ -1,2 +1,132 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  OnFrame.swift
+//  OnFrame
+//
+//  Created by Kristian Trenskow on 24/04/2025.
+//
+
+import SwiftUI
+
+@MainActor
+struct OnFrameDisplayLinkView: UIViewRepresentable {
+
+	let action: @MainActor @Sendable (TimeInterval, TimeInterval) -> Void
+
+	class Coordinator {
+
+		var displayLink: CADisplayLink? {
+			didSet {
+				oldValue?.invalidate()
+				self.displayLink?.add(
+					to: .main,
+					forMode: .common)
+			}
+		}
+
+		let action: @MainActor @Sendable (TimeInterval, TimeInterval) -> Void
+
+		init(
+			action: @MainActor @Sendable @escaping (TimeInterval, TimeInterval) -> Void
+		) {
+			self.action = action
+		}
+
+		deinit {
+			self.displayLink = nil
+		}
+
+		@MainActor
+		@objc func displayLinkAction(
+			displayLink: CADisplayLink
+		) {
+			self.action(
+				displayLink.timestamp,
+				displayLink.targetTimestamp)
+		}
+
+	}
+
+	func makeCoordinator() -> Coordinator {
+
+		let coordinator = Coordinator(
+			action: self.action)
+
+		coordinator.displayLink = CADisplayLink(
+			target: coordinator,
+			selector: #selector(
+				Coordinator.displayLinkAction(displayLink:)))
+
+		return coordinator
+
+	}
+
+	func makeUIView(context: Context) -> UIView {
+
+		let view = UIView()
+
+		view.backgroundColor = .clear
+
+		return view
+
+	}
+
+	func updateUIView(
+		_ uiView: UIView,
+		context: Context
+	) {}
+
+}
+
+@MainActor
+struct OnFrameModifier: ViewModifier {
+
+	@State private var lastTimeStamp: TimeInterval = 0
+
+	let action: (@MainActor @Sendable (TimeInterval, TimeInterval) -> Void)?
+
+	init(
+		action: (@MainActor @Sendable (TimeInterval, TimeInterval) -> Void)?
+	) {
+		self.action = action
+	}
+
+	func body(content: Content) -> some View {
+		content
+			.background(
+				OnFrameDisplayLinkView(
+					action: { oldTimeInterval, newTimeInterval in
+						self.lastTimeStamp = newTimeInterval
+						self.action?(oldTimeInterval, newTimeInterval)
+					}))
+			.environment(
+				\.timestamp,
+				 self.lastTimeStamp)
+	}
+
+}
+
+extension View {
+
+	@MainActor
+	public func onFrame(
+		_ action: (@MainActor @Sendable (TimeInterval, TimeInterval) -> Void)? = nil
+	) -> some View {
+		self.modifier(
+			OnFrameModifier(
+				action: action))
+	}
+
+}
+
+public struct TimestampKey: EnvironmentKey {
+	public static let defaultValue: TimeInterval = 0
+}
+
+extension EnvironmentValues {
+
+	public var timestamp: TimeInterval {
+		get { self[TimestampKey.self] }
+		set { self[TimestampKey.self] = newValue }
+	}
+
+}
